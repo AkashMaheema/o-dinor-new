e0e78fdc823d74090225b7c368bcad1e5b1b0387
Namalekanayaka <namalekanayake412@gmail.com>
2024-08-30 23:40:55 +0530

    update2.1

--- o-Dinor_front/configdb.php
+++ o-Dinor_front/configdb.php
@@ -1,11 +1,12 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "Plus888@";
+$password = "";
+$port = 4306;
 $dbname = "o_dinor";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname,$port);
 
 // Check connection
 if ($conn->connect_error) {
